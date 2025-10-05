2b861bfefe53fe9908ba9e808418c24e03e0cf05
Jaxx347 <jaxx347@gmail.com>
2025-10-04 21:45:48 -0500

    10/4 - jl

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -28,16 +28,36 @@ const readline = require('readline-sync');
 
 const hasTorch = true;
 const hasMap = false;
+var hasSword = false
 
 console.log("You see two paths: one leads to the mountains, the other to the village.");
 const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
 
 if (choice === "mountains" && hasTorch) {
   console.log("You safely navigate through the dark mountains.");
+  mountChoice = readline.question("Your path through the mountain comes to an abandoned camp next to a cave, do you 'search' the camp, enter the 'cave' or go 'back'?")
+    if (mountChoice === "back") {
+      console.log("You make your way back to the village.");
+    
+    } else if (mountChoice === "search") {
+      hasSword = true
+      console.log("You search the camp and find a sword!");
+    }
+      else if (mountChoice === "cave" && hasTorch) {
+      console.log("You navigate through the cave with your torch and find a sleeping dragon.");
+    }
+  
 } else if (choice === "mountains" && !hasTorch) {
   console.log("It's too dark to proceed. You decide to turn back.");
 } else if (choice === "village" || hasMap) {
   console.log("You find your way to the village.");
+  villageChoice = readline.question("You enter the village and see a tavern and blacksmith, do you go to the 'tavern' or go to the 'blacksmith'?")
+  if (villageChoice === "tavern") {
+    console.log("You enter the tavern, there is a lone tavern keeper polishing a tankard.")
+  }
+  else if (villageChoice === "blacksmith") {
+    console.log("You approach the blacksmith, he is working a piece of iron on an anvil.");
+  }
 } else {
   console.log("You get lost and wander aimlessly.");
 }
